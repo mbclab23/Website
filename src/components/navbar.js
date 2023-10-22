83214029b8c89ec19823ac09e0775ee93cc2bb9b
pandudwinugraha <pandudwinugraha01@gmail.com>
2023-10-22 18:12:54 +0700

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,25 @@
 import style from "@/components/style/navbar.module.css";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <>
       <div className={`${style.container}`}>
-        <h1>navbar</h1>
-        <h1>MBC Lab</h1>
-        <h1>test</h1>
-        <h1>test</h1>
+        <div className="m-auto">
+        <Image
+        src="/image/MBC.png"
+        width={250}
+        height={250}
+        alt="Picture of the author"
+    />
+        </div>
+        <div className={`${style.list}`}>
+          <h1>Beranda</h1>
+          <h1>Kegiatan</h1>
+          <h1>projek</h1>
+          <h1>Tentang</h1>
+          <h1>Lainnya</h1>
+        </div>
       </div>
     </>
   );
